9bee0deb9e6283d042aba249de4eb53310f5e97c
taejundev <taejunpark.dev@gmail.com>
2020-10-26 18:37:14 +0900

    Add gh-pages: https://www.gatsbyjs.com/docs/how-gatsby-works-with-github-pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,7 @@ require(`dotenv`).config({
 const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE
 
 module.exports = {
+  pathPrefix: "/devMemoBlog",
   siteMetadata: {
     siteTitleAlt: `Minimal Blog - Gatsby Theme`,
   },
